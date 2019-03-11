7d419a2660e0d9e8270c59f0685d549b47128c94
Titouan HERVIEU <43231871+Noituat@users.noreply.github.com>
2019-03-11 09:46:04 +0100

    Ajout des vues suivantes :     -> Dashboard     -> Emploi du temps     -> Projets en cours     -> Fichiers     -> Login
    Les boutons ont été ajoutés au fichier 'app.component.html'. Un style 
    SCSS TEMPORAIRE leur a été attribué afin de rendre leur placement 
    initial plus simple.
    
    Lors de la reprise du projet, je prévois de m'attaquer au 
    DashboardComponent afin d'afficher un agenda qui sera relié à une base 
    de données.
    Cet agenda sera juste affiché sur le dashboard comme aide visuelle. La 
    base de données sera modifiable par la vue 'AgendaComponent'.

--- src/app/projects/projects.component.spec.ts
+++ src/app/projects/projects.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MatrixDisplayComponent } from './matrix-display.component';
+import { ProjectsComponent } from './projects.component';
 
-describe('MatrixDisplayComponent', () => {
-  let component: MatrixDisplayComponent;
-  let fixture: ComponentFixture<MatrixDisplayComponent>;
+describe('ProjectsComponent', () => {
+  let component: ProjectsComponent;
+  let fixture: ComponentFixture<ProjectsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MatrixDisplayComponent ]
+      declarations: [ ProjectsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MatrixDisplayComponent);
+    fixture = TestBed.createComponent(ProjectsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
