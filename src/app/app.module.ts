68084c37ac3096b747363618ba41c398918aebcb
Titouan HERVIEU <titouan.hervieu@gmail.com>
2019-03-14 19:57:55 +0100

    update 14/03/2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { AgendaComponent } from './agenda/agenda.component';
+import { AgendaViewerComponent } from './agenda-viewer/agenda-viewer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { CookieService } from 'ngx-cookie-service';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { FileViewerComponent } from './file-viewer/file-viewer.component';
 import { LoginComponent } from './login/login.component';
-import { AgendaViewerComponent } from './agenda-viewer/agenda-viewer.component';
+import { NgModule } from '@angular/core';
+import { ProjectsComponent } from './projects/projects.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +22,14 @@ import { AgendaViewerComponent } from './agenda-viewer/agenda-viewer.component';
     ProjectsComponent,
     FileViewerComponent,
     LoginComponent,
-    AgendaViewerComponent
+    AgendaViewerComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CookieService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
